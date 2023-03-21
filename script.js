23e102fec58b3cdc286f78fbafb3b256fcd254e5
Gemilang Karunia Gusti <125884682+gilangGusti@users.noreply.github.com>
2023-03-21 23:21:55 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+// Tugas 5 NO. 1
+// Array adalah tipe data yang berisi kumpulan dari nilai atau tipe data lain. Elemen dari array bisa bertipe data string, number, dan juga boolean dalam sebuah array yang sama. Array dibuat dengan tanda kurung siku […] . Indeks array selalu dimulai dari nol.
+// Object adalah cara pemodelan dalam bentuk nyata. Bisa juga diartikan sebagai benda atau hal yang akan dibahas.
